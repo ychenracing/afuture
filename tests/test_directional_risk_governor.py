31f5246ee1cbf398f953a7b112e78330278af083
ychenracing <ychenracing@163.com>
2026-08-23 01:08:16 +0800

    test: require non-increasing directional policy wrapper

--- tests/test_directional_risk_governor.py
+++ tests/test_directional_risk_governor.py
@@ -1,4 +1,7 @@
-from afuture.directional_risk import DirectionalRiskGovernor
+from afuture.directional_risk import (
+    DirectionalRiskGovernor,
+    DirectionalRiskScaledPolicy,
+)
 from afuture.state import RuntimeState, StateStore
 
 
@@ -16,6 +19,28 @@ def test_directional_risk_governor_keeps_scale_inside_one_and_never_increases_gr
     for returns in ([], [0.10], [0.10, -0.10], [-0.50, 0.50]):
         scale = governor.scale(returns)
         assert 0.0 < scale <= 1.0
+
+
+def test_scaled_policy_only_reduces_existing_target_weights():
+    class _Policy:
+        def target_weights(self, *args, **kwargs):
+            return {"A": 1.2, "CU": -0.8}
+
+    completed = [-0.04, 0.01]
+    policy = DirectionalRiskScaledPolicy(
+        _Policy(),
+        completed_returns_provider=lambda: tuple(completed),
+    )
+    assert policy.target_weights(object(), object()) == {
+        "A": 0.3,
+        "CU": -0.2,
+    }
+
+    completed[:] = [0.01, 0.01]
+    assert policy.target_weights(object(), object()) == {
+        "A": 1.2,
+        "CU": -0.8,
+    }
 
 
 def test_runtime_state_persists_completed_directional_return_history_and_daily_circuit(tmp_path):
